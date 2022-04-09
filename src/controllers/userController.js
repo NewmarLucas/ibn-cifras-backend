bfa37df5aa5542e845256758657fba99685b07f8
NewmarLucas <newmarlucas@gmail.com>
2022-04-09 12:11:47 -0400

    feat: create jwt middleware; create login and register user routes

---
+++ src/controllers/userController.js
@@ -0,0 +1,27 @@
+const { login, register } = require('../models/userModel')
+
+module.exports.login = async (req, res) => {
+  const { email, password } = req.body
+  try {
+    const user = await login(email, password)
+    if (user === 'Invalid user') {
+      return res.status(401).json({ msg: user })
+    }
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(500).json({ error: error })
+  }
+}
+
+module.exports.register = async (req, res) => {
+  const data = req.body
+  try {
+    const user = await register(data)
+    if (user === 'Email already exists') {
+      return res.status(401).json({ msg: user })
+    }
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(500).json({ error: error })
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const routes = express.Router()
+const admLoginAuth = require('./middlewares/jwtAuth')
 
 const MusicController = require('./controllers/musicController')
+const UserController = require('./controllers/userController')
 
 // Admin routes
-routes.get('/admin/musics', MusicController.list)
-routes.post('/admin/music', MusicController.register)
+routes.post('/admin', UserController.login)
+routes.post('/admin/register', UserController.register)
+routes.get('/admin/musics', admLoginAuth, MusicController.list)
+routes.post('/admin/music', admLoginAuth, MusicController.register)
 
 module.exports = routes
