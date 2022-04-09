65b345fb74ac55bf5e8789196b35e3346f8b9ed1
NewmarLucas <newmarlucas@gmail.com>
2022-04-09 10:56:23 -0400

    feat: initializing jwt configuration

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 require('dotenv').config()
-const express = require('express');
-const cors = require('cors');
-const routes = require('./routes');
+const jwt = require('jsonwebtoken')
+const express = require('express')
+const cors = require('cors')
+const routes = require('./routes')
 
-const port = process.env.PORT || 3333;
-const app = express();
+const port = process.env.PORT || 3333
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(routes);
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
 require('../src/database/connection/connection')
 
-app.listen(port);
+app.listen(port)
